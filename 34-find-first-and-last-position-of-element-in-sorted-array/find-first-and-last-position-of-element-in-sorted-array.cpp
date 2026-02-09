556a48e65f2947ffe2003826973c80f7a71fe85f
Amit Yadav <aymaidtav@gmail.com>
2026-02-09 23:11:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (18.66%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -10,11 +10,8 @@ public:
         if (ans.empty()) {
             return {-1, -1};
         }
-        vector<int>val;
-        val.push_back(ans[0]);
-        val.push_back(ans[ans.size()-1]);
+        
 
-
-        return val;
+        return {ans.front(),ans.back()};
     }
 };
