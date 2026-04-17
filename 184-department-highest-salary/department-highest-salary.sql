12375d4aaec6ed7351cfb084a01eceffa3f86283
Amit Yadav <aymaidtav@gmail.com>
2026-04-17 10:25:32 +0530

    Time: 772 ms (38.23%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,5 +1,5 @@
 select d.name as Department , e.name as Employee ,e.salary
-from(select *,rank() over(partition by departmentId order by salary desc) 
+from(select *,dense_rank() over(partition by departmentId order by salary desc) 
 as rnk from Employee ) e 
 join Department d on e.DepartmentId=d.id 
 where e.rnk=1;
