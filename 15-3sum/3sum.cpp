ef4b13a983dee333bd7907f65ab16c2465edf60e
Amit Yadav <aymaidtav@gmail.com>
2026-08-27 02:01:57 +0530

    Time: 39 ms (94.68%) | Memory: 29 MB (73.11%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n= nums.size();
-        vector<vector<int>>result;
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]){
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> result;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int left = i+1;
-            int right = n-1;
-            int sum = -1*nums[i];
-            while(left < right ){
-                int s = nums[left]+nums[right];
-
-                if(s==sum){
-                    result.push_back({nums[i],nums[left],nums[right]});
+            int left =i+1;
+            int right = n - 1;
+            int s = -1 * nums[i];
+            
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (s == sum) {
+                    result.push_back({nums[i], nums[left], nums[right]});
                     left++;
                     right--;
-                    while(left<n && nums[left]==nums[left-1]){
+                    while (left < n && nums[left] == nums[left - 1]) {
                         left++;
                     }
-                    while(right>=0 && nums[right]==nums[right+1]){
+                    while (right>=0 && nums[right] == nums[right + 1]) {
                         right--;
                     }
 
-                }
-                else if (s<sum){
+                } else if (sum<s) {
                     left++;
-                }else{
+                } else {
                     right--;
-
                 }
             }
-        
-
         }
         return result;
-        
     }
 };
